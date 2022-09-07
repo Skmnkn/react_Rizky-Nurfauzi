deee734bbf1e0e045b97c369b202e6572029ba5b
Rizky Nurfauzi <kridwan95@gmail.com>
2022-09-07 16:38:32 +0700

    task/Javascript : Completed Task

---
+++ 08_JavaScript-Refreshment/Praktikum/task3.js
@@ -0,0 +1,5 @@
+const foo = ["Budi", "Sista", "Ayu"];
+const [a, b, c] = foo;
+console.log(`${a} tersimpan dalam variable a`);
+console.log(`${b} tersimpan dalam variable b`);
+console.log(`${c} tersimpan dalam variable c`);
