9505d5469297dc3ac6917088130bdd7c08b32b5a
Rizky Nurfauzi <kridwan95@gmail.com>
2022-12-02 23:13:38 +0700

    task/Weekly-02 : Install Dependencies

--- 28_Weekly_Task/weekly2/project/src/App.js
+++ 28_Weekly_Task/weekly2/project/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+import { HomePage } from "./pages/Home";
+import { Navbar } from "./components/navbar";
+import { Footer } from "./components/Footer";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <HomePage />
+      <Footer />
+      <Helmet>
+        <script>
+          var scene = document.getElementById("scene"); var parallax = new
+          Parallax(scene);
+        </script>
+
+        <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+      </Helmet>
+    </>
   );
 }
 
